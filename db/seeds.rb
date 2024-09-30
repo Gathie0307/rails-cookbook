79e78c06ec9ebdd8199a7c0e2eeab71ceb6fe43e
Gayathri Krishnan <iamkgayu@gmail.com>
2024-09-30 13:43:16 +0100

    Added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Recipe.create(name: "butter chicken", description: "Indian Curry", image_url: "https://ichef.bbci.co.uk/food/ic/food_16x9_1600/recipes/butter_chicken_01541_16x9.jpg", rating: 5.0)
+Recipe.create(name: "Pitta pockets", description: "A no-cook quick and simple snack that's ideal for an energy-boosting refuel when doing exercise or playing sport", image_url: "https://www.heartuk.org.uk/images/recipes/spicy-chicken-pittas_cropped.jpg", rating: 3.4)
+Recipe.create(name: "Crispy noodle pancake", description: "Speedy to make, the dish takes under 30 minutes from prep to plate", image_url: "https://i0.wp.com/isinginthekitchen.com/wp-content/uploads/2013/03/dsc_00031.jpg", rating: 4.0)
+Recipe.create(name: "chicken biriyani", description: "A great one-pot rice dish", image_url: "https://healux.in/wp-content/uploads/2020/11/ChickenBiryani.jpg", rating: 4.5)
